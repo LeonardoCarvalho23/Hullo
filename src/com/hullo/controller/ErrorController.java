6c70408c9ff314a147721b40e9254c5b38894732
Erick Paulino <Erick Paulino>
2017-04-11 17:35:33 -0300

    Página de erro generica adicionada

--- src/com/hullo/controller/ErrorController.java
+++ src/com/hullo/controller/ErrorController.java
@@ -3,6 +3,7 @@ package com.hullo.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.portlet.ModelAndView;
@@ -11,32 +12,31 @@ import org.springframework.web.portlet.ModelAndView;
 public class ErrorController {
  
     @RequestMapping(value = "errors", method = RequestMethod.GET)
-    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
-         
-        ModelAndView errorPage = new ModelAndView("errorPage");
+    public String renderErrorPage(HttpServletRequest httpRequest, Model model) {
+         System.out.println("Chegou no error controler");
         String errorMsg = "";
         int httpErrorCode = getErrorCode(httpRequest);
- 
+        System.out.println("o httperrorcode é "+httpErrorCode);
         switch (httpErrorCode) {
             case 400: {
-                errorMsg = "Http Error Code: 400. Bad Request";
+                errorMsg = "Código de erro: 400. Requisição inválida.";
                 break;
             }
             case 401: {
-                errorMsg = "Http Error Code: 401. Unauthorized";
+                errorMsg = "Código de erro: 401. Você não tem permissão.";
                 break;
             }
             case 404: {
-                errorMsg = "Http Error Code: 404. Resource not found";
+                errorMsg = "Código de erro: 404. Página não encontrada.";
                 break;
             }
             case 500: {
-                errorMsg = "Http Error Code: 500. Internal Server Error";
+                errorMsg = "Código de erro: 500. Erro interno do servidor.";
                 break;
             }
         }
-        errorPage.addObject("errorMsg", errorMsg);
-        return errorPage;
+        model.addAttribute("errorMsg", errorMsg);
+        return "errorPage";
     }
      
     private int getErrorCode(HttpServletRequest httpRequest) {
